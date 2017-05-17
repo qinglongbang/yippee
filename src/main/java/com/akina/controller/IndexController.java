570b359e36436e78fbd736d82848623c9c90d0e9
野生可乐 <13550196274@163.com>
2017-05-17 14:15:36 +0800

    前端页面

--- src/main/java/com/akina/controller/IndexController.java
+++ src/main/java/com/akina/controller/IndexController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @Describe ： 全系統的歡迎頁面  返回的是文字格式， 必须用RestController  注解，如果返回的是页面就要使用Controller了
  */
 @Controller
-public class indexController {
+public class IndexController {
     /**
      * 跳轉至歡迎頁面
      *
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -13,6 +13,29 @@ $(document).ready(function () {
 function init() {
     //手机菜单折叠初始化
     $(".button-collapse").sideNav();
-    $('.modal').modal();
+    //初始化loading组件
+    $('.modal').modal({
+            dismissible: false, // 模态可以通过点击外部的模态
+            opacity: 1, // 模态背景不透明度
+            in_duration: 300, // 过渡期动画
+            out_duration: 200, // 转型期的持续时间
+            starting_top: '4%', // 启动顶部样式属性
+            ending_top: '10%', //结束顶端样式属性
+            ready: function (modal, trigger) {
+                // 模式打开回调。模态和触发参数可用。
+            },
+            complete: function () {
+                //模态关闭的回调
+            }
+        }
+    );
+
+
+    //打开loding
+    // $('#loding').modal('open');
+    //关闭loding
+    //$('#loding').modal('close');
 }
+
+
 
